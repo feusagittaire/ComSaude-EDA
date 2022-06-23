778221c5a8a882eebf3e299384f8c49693c2e7f2
Arthur Lopes <75845199+feusagittaire@users.noreply.github.com>
2022-06-22 23:15:19 -0300

    Add files via upload

---
+++ Pagina_inicial.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+from pandas_profiling import ProfileReport
+from streamlit_pandas_profiling import st_profile_report
+
+
+#Web App Title
+st.markdown('''
+# **Análise exploratória de dados do CoronaJor**
+
+Essa é uma aplicação web criada através da biblioteca Streamlit (https://docs.streamlit.io/l) em linguagem Python por Arthur Lopes.
+O intuito da aplicação é tornar mais fácil e acessível a análise exploratória de dados para pesquisadores(as) leigas em programação.
+
+Boa pesquisa!
+''')
+
+
+
+
