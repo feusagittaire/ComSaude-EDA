ac5b2678b0b9edc4cc1012cfc7b39470146189b6
Arthur Lopes <75845199+feusagittaire@users.noreply.github.com>
2022-06-23 22:39:30 -0300

    Update Analise_exploratória.py

--- pages/Analise_exploratória.py
+++ pages/Analise_exploratória.py
@@ -59,9 +59,9 @@ if uplouded_file is not None:
        
         timepossibility = st.selectbox('Seu DataFrame contém coluna de data? É possível fazer análise temporal com os dados que possui?', ['Não', 'Sim'])
         if timepossibility == 'Sim':
-            timecolumn = ''
             timecolumn = st.text_input('Qual o nome da coluna? (Você pode adquirir essa informação na sessão DataFrame. Olha na tabela qual o nome, copia, e cola aqui')
             timeformat = st.selectbox('Sua data está em qual formato?', ['dia/mes/ano', 'dia/mes/ano hora/min/segudos'])
+            st.write(len(timecolumn))
             if len(timecolumn) >= 1:
                 if timeformat == 'dia/mes/ano hora/min/segudos':
                     df[timecolumn] = pd.to_datetime(df[timecolumn], utc = True)
